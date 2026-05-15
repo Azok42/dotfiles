2d16933fa04df3c827a2d7054aed50fc1f3ae643
Azok42 <anton.hackner@sz-ybbs.ac.at>
2026-05-15 09:27:38 +0200

    add *working* remap for error diagnostics

--- nvim/lua/meh/remap.lua
+++ nvim/lua/meh/remap.lua
@@ -3,4 +3,4 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 
 -- lsp remaps
---vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition())
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
